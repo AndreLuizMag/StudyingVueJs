2308439b7a46705eb64cf7883167e806dce00d04
AndreLuizMag <andreluizmagjr@outlook.com>
2021-10-07 17:52:00 -0300

    Implement more elements

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <p>Footer</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="./style.scss" scoped />
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <p>Header</p>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="./style.scss" scoped />
